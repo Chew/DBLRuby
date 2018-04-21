9048928f5956d3ae93abcb0e6b2457f135200eec
Chewsterchew <chew@chew.pw>
2018-04-20 21:40:16 -0500

    Stat commands for stating.

---
+++ lib/dblruby/stats.rb
@@ -0,0 +1,13 @@
+# Stats and stuff, ya know.
+class DBLRuby::Stats
+  # Initialize the stats
+  def initialize(apikey, id)
+    @api = apikey
+    @id = id
+  end
+
+  # get the server count
+  def getservercount
+    'no u'
+  end
+end
