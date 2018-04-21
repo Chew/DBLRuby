fbca3e13a7008afb75d8508ebbb6e4480ce5099e
Chewsterchew <chew@chew.pw>
2018-04-21 08:21:09 -0500

    Actually get server count.

--- lib/dblruby/stats.rb
+++ lib/dblruby/stats.rb
@@ -1,13 +1,15 @@
 # Stats and stuff, ya know.
 class DBLRuby::Stats
   # Initialize the stats
+  # Program does this automatically when running DBLRuby.new.
   def initialize(apikey, id)
     @api = apikey
     @id = id
   end
 
-  # get the server count
+  # Get the server count, returns an int.
   def getservercount
-    'no u'
+    url = "https://discordbots.org/api/bots/#{@id}"
+    JSON.parse(RestClient.get(url))['server_count'].to_i
   end
 end
