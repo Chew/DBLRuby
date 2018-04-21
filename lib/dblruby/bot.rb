4843e4faf3aefaa3da6f2d0dc8789d0d336a4f67
Chewsterchew <chew@chew.pw>
2018-04-21 17:47:28 -0500

    Document things! REE!

--- lib/dblruby/bot.rb
+++ lib/dblruby/bot.rb
@@ -20,42 +20,52 @@ class DBLRuby::Bot
     @data['github']
   end
 
+  # Get the bot's website link.
   def website
     @data['website']
   end
 
+  # Get the bot's Long Description.
   def longdesc
     @data['longdesc']
   end
 
+  # Get the bot's Short Description.
   def shortdesc
     @data['shortdesc']
   end
 
+  # Get the bot's prefix.
   def prefix
     @data['prefix']
   end
 
+  # Get the bot's library.
   def lib
     @data['lib']
   end
 
+  # Get the bot's Client ID.
   def clientid
     @data['clientid']
   end
 
+  # Get the bot's avatar.
   def avatar
     @data['avatar']
   end
 
+  # Get's the bot's avatar as an img, ready to be used in image linking.
   def avatar_img
     "https://cdn.discordapp.com/avatars/#{id}/#{avatar}.webp?size=1024"
   end
 
+  # Get the bot's ID.
   def id
     @data['id']
   end
 
+  # Get the bot's discriminator.
   def discriminator
     @data['discriminator']
   end
@@ -63,18 +73,22 @@ class DBLRuby::Bot
   alias discrim discriminator
   alias tag discriminator
 
+  # Get the bot's username.
   def username
     @data['username']
   end
 
+  # Get the bot's creation date (on the list).
   def date
     @data['date']
   end
 
+  # Get the bot's support server invite code.
   def support
     @data['support']
   end
 
+  # Get the bot's support server link, ready for clicking.
   def support_link
     "https://discord.gg/#{support}"
   end
@@ -87,28 +101,33 @@ class DBLRuby::Bot
   alias guild_count server_count
   alias server server_count
 
+  # Get the bot's "This Bot Powers the following Servers"
   def guilds
     @data['guilds']
   end
 
   alias servers guilds
 
+  # Get the bot's shards.
   def shards
     @data['shards']
   end
 
+  # Get the bot's monthly votes.
   def monthlypoints
     @data['monthlyPoints']
   end
 
   alias monthlyvotes monthlypoints
 
+  # Get the bot's total votes.
   def points
     @data['points']
   end
 
   alias votes points
 
+  # Get the bot's certified status.
   def certifiedbot
     @data['certifiedBot']
   end
@@ -117,20 +136,24 @@ class DBLRuby::Bot
   alias certified certifiedbot
   alias certifiedbot? certifiedbot
 
+  # Get the bot's owners.
   def owners
     @data['owners']
   end
 
+  # Get the bot's sorting tags.
   def tags
     @data['tags']
   end
 
+  # Get the bot's legacy status.
   def legacy
     @data['legacy']
   end
 
   alias legacy? legacy
 
+  # Debug method to force a bot error.
   def fail
     raise DBLRuby::Errors::NoData, 'The API didn\'t return anything!'
   end
