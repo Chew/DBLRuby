53a7022e29e8e420acb0f14464ef1728c704b8d6
Chewsterchew <chew@chew.pw>
2018-04-20 21:40:09 -0500

    Start making a base!

--- lib/dblruby.rb
+++ lib/dblruby.rb
@@ -1,9 +1,18 @@
 # All DBLRuby functionality, whether extended or just here.
 class DBLRuby
   # Initialize a new DBL API, via a key.
-  def initialize(apikey)
+  def initialize(apikey, id)
     @api = apikey
+    @id = id
+    makestats
   end
+
+  # Initialize stats
+  def makestats
+    @stats = Stats.new(@api, @id)
+  end
+
+  attr_reader :stats
 end
 
 require 'dblruby/stats'
