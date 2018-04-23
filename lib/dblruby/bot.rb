97e6acf942753c07ef38e20e6fe65aa5d4045a8c
Chewsterchew <chew@chew.pw>
2018-04-22 21:03:36 -0500

    Document bot methods better and return some things as integers

--- lib/dblruby/bot.rb
+++ lib/dblruby/bot.rb
@@ -11,61 +11,73 @@ class DBLRuby::Bot
   attr_reader :data
 
   # Get the bot's invite link.
+  # @return [String] the bot's invite link.
   def invite
     @data['invite']
   end
 
   # Get the bot's GitHub Repo link.
+  # @return [String] the bot's GitHub Repo link.
   def github
     @data['github']
   end
 
   # Get the bot's website link.
+  # @return [String] the bot's website link.
   def website
     @data['website']
   end
 
   # Get the bot's Long Description.
+  # @return [String] the bot's Long Description.
   def longdesc
     @data['longdesc']
   end
 
   # Get the bot's Short Description.
+  # @return [String] the bot's Short Description.
   def shortdesc
     @data['shortdesc']
   end
 
   # Get the bot's prefix.
+  # @return [String] the bot's prefix.
   def prefix
     @data['prefix']
   end
 
   # Get the bot's library.
+  # @return [String] the bot's library.
   def lib
     @data['lib']
   end
 
   # Get the bot's Client ID.
+  # @return [Integer] the bot's client id.
   def clientid
-    @data['clientid']
+    @data['clientid'].to_i
   end
 
   # Get the bot's avatar.
+  # @return [String] the bot's avatar.
   def avatar
     @data['avatar']
   end
 
   # Get's the bot's avatar as an img, ready to be used in image linking.
+  # @return [String] the bot's avatar image url.
   def avatar_img
     "https://cdn.discordapp.com/avatars/#{id}/#{avatar}.webp?size=1024"
   end
 
   # Get the bot's ID.
+  # @return [Integer] the bot's id.
   def id
-    @data['id']
+    @data['id'].to_i
   end
 
   # Get the bot's discriminator.
+  # @return [String] the bot's discriminator.
   def discriminator
     @data['discriminator']
   end
@@ -74,39 +86,46 @@ class DBLRuby::Bot
   alias tag discriminator
 
   # Get the bot's username.
+  # @return [String] the bot's username.
   def username
     @data['username']
   end
 
   # Returns the bot's distinct, which is the Username and Discriminator.
+  # @return [String] the bot's distinct.
   def distinct
     "#{username}#{tag}"
   end
 
   # Get the bot's creation date (on the list).
+  # @return [String] the bot's date.
   def date
     @data['date']
   end
 
   # Get the bot's support server invite code.
+  # @return [String] the bot's support server code.
   def support
     @data['support']
   end
 
   # Get the bot's support server link, ready for clicking.
+  # @return [String] the bot's support server link.
   def support_link
     "https://discord.gg/#{support}"
   end
 
   # Get the bot's server count
+  # @return [Integer] the bot's server count.
   def server_count
-    @data['server_count']
+    @data['server_count'].to_i
   end
 
   alias guild_count server_count
   alias server server_count
 
   # Get the bot's "This Bot Powers the following Servers"
+  # @return [Array<String>] the bot's guilds.
   def guilds
     @data['guilds']
   end
@@ -114,25 +133,29 @@ class DBLRuby::Bot
   alias servers guilds
 
   # Get the bot's shards.
+  # @return [Array<String>] the bot's shards.
   def shards
     @data['shards']
   end
 
   # Get the bot's monthly votes.
+  # @return [Integer] the bot's monthly points/votes.
   def monthlypoints
-    @data['monthlyPoints']
+    @data['monthlyPoints'].to_i
   end
 
   alias monthlyvotes monthlypoints
 
   # Get the bot's total votes.
+  # @return [Integer] the bot's total points/votes.
   def points
-    @data['points']
+    @data['points'].to_i
   end
 
   alias votes points
 
   # Get the bot's certified status.
+  # @return [true, false] the bot's certified status.
   def certifiedbot
     @data['certifiedBot']
   end
@@ -142,16 +165,19 @@ class DBLRuby::Bot
   alias certifiedbot? certifiedbot
 
   # Get the bot's owners.
+  # @return [Array<String>] the bot's owners in an array.
   def owners
     @data['owners']
   end
 
   # Get the bot's sorting tags.
+  # @return [Array<String>] the bot's tags in an array.
   def tags
     @data['tags']
   end
 
   # Get the bot's legacy status.
+  # @return [true, false] the bot's legacy status.
   def legacy
     @data['legacy']
   end
