49c69aa8f934b3f0281d85514dc32cc329e9d3c9
Chewsterchew <chew@chew.pw>
2018-04-28 16:42:44 -0500

    Removed un-used error

--- lib/dblruby/errors.rb
+++ lib/dblruby/errors.rb
@@ -1,8 +1,5 @@
 # Errors are cool.
 module DBLRuby::Errors
-  # Ran if No Data is returned.
-  class NoData < RuntimeError; end
-
   # Ran if a 404 error is returned in #loadbot
   class InvalidBot < RuntimeError; end
 
